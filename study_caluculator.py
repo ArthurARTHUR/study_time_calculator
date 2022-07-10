6a094115fb044fadc99e8baeeb253ee3e094d3cb
ArthurARTHUR <arthur1952017828@gmail.com>
2022-07-10 18:07:34 +0800

    Add files via upload

--- study_caluculator.py
+++ study_caluculator.py
@@ -2,11 +2,20 @@ import os
 from datetime import datetime
 import time
 import re
+import numpy as np
 
 
 def main():
-    study_logging()
+    key = input('''======STUDY RECORD APPLICATION ======
 
+1.Record your study
+2.Show how many minutes you have studied today
+Please indicate your choice(default is 1):''')
+    print('\n')
+    if key != "2":
+        study_logging()
+    else:
+        aggregate_study_time()
 
 # change the directory to where we save the study logs.
 def change_directory():
@@ -41,15 +50,17 @@ def study_logging():
     print(start_logging)
 
     try:
+        minutes = 5
         for i in range(1, 13):# alarm for every 5 minutes
             time.sleep(300)
-            print(f'This is the {i} five-minute!')
-         
+            print(f'{minutes} minutes!')
+            minutes = minutes + 5
+        print('Session completed!')
         # demenstrate the end time of your study and the total study time of this session.
         end_time = datetime.now()
         end_logging = f'End time: {end_time}'
         time_diff = (end_time - start_time).seconds
-        total_study_time_logging = f'Total study minutes:{round(time_diff/60,0)}'
+        total_study_time_logging = f'Total study minutes:{int(time_diff/60)}'
         
         print(end_logging)
         print(total_study_time_logging)
@@ -67,7 +78,25 @@ def study_logging():
         with open(study_logging_file, 'a') as f:
             f.write(logging)
             f.write("\n\n")
+
+
+def aggregate_study_time():
+    change_directory()
+    date_prefix = datetime.now().strftime('%Y-%m-%d')
+    study_logging_file = date_prefix+'_study_logging.txt'
     
+    if not os.path.exists(study_logging_file):
+        print("Could not find the records!")
+        exit()
+    else:
+        reg = r'minutes: (\d+)'
+        with open(study_logging_file, 'r') as f:
+            lines = f.readlines()
+        lines = ','.join(lines)
+        study_in_minutes = re.findall(reg, lines)
+        study_in_minutes = [ int(i) for i in study_in_minutes]
+        result = np.sum(study_in_minutes)
+        print(f'You have study {result} minutes today') 
                     
 if __name__ == "__main__":
     main()
